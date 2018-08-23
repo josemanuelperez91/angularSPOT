67e99c5198e3fd5f9c39caaea898acdd6a47bcc9
Jose Manuel Pérez <josemanuelperez@comovapp.es>
2018-08-23 17:38:56 +0200

    Elementos creados y rutas

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styles: []
+})
+
+export class HomeComponent implements OnInit {
+
+  constructor( private http: HttpClient) {
+
+    this.http.get('').subscribe( response => {
+
+    });
+
+  }
+
+  ngOnInit() {
+  }
+
+}
